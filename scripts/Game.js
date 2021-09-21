e43da51aa4342c9a24c0aed79d2b6d4b121ab5f2
Gabriel Frasson <bielfc27@gmail.com>
2021-09-21 02:22:38 -0300

    Aplicando configurações recebidas da tela de início

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,18 +1,48 @@
 class Game {
-    constructor() {
-        this.numberOfTiles = 30;
-        this.points = 0;
-        this.intervalInMiliseconds = 50;
+    difficultiesConfigurations = Object.freeze({
+        EASY: {
+            numberOfTiles: 10,
+            intervalInMiliseconds: 100
+        },
+        MEDIUM: {
+            numberOfTiles: 15,
+            intervalInMiliseconds: 85
+        },
+        HARD: {
+            numberOfTiles: 20,
+            intervalInMiliseconds: 63
+        },
+        EXPERT: {
+            numberOfTiles: 30,
+            intervalInMiliseconds: 50
+        },
+    });
 
+    constructor() {
+        this.score = 0;
+        this.highestScore = 0;
+
+        this.scoreHTML = document.querySelector('#score');
+        this.highestScoreHTML = document.querySelector('#highest-score');
+    }
+
+    startGame(difficulty) {
+        // Difficulty configurations
+        if (this.difficultiesConfigurations.hasOwnProperty(difficulty)) {
+            this.numberOfTiles = this.difficultiesConfigurations[difficulty].numberOfTiles;
+            this.intervalInMiliseconds = this.difficultiesConfigurations[difficulty].intervalInMiliseconds;
+        } else {
+            this.numberOfTiles = this.difficultiesConfigurations.EASY.numberOfTiles;
+            this.intervalInMiliseconds = this.difficultiesConfigurations.EASY.intervalInMiliseconds;
+        }
+
+        // Instances
         this.canvasDraw = new CanvasDraw(this.numberOfTiles);
         this.board = new Board(this.canvasDraw);
         this.apple = new Apple(this.canvasDraw, this.numberOfTiles);
         this.snake = new Snake(this.canvasDraw, this.numberOfTiles, this.apple);
 
-        this.startGame();
-    }
-
-    startGame() {
+        // Game run
         this.gameInterval = setInterval(() => {
             this.snake.updateSnake();
            
@@ -21,6 +51,14 @@ class Game {
             }
 
             if (this.apple.eaten) {
+                this.score++;
+                this.scoreHTML.innerHTML = this.score;
+
+                if (this.score > this.highestScore) {
+                    this.highestScore++;
+                    this.highestScoreHTML.innerHTML = this.highestScore;
+                }
+
                 this.apple.randomizePositions(this.snake);
                 this.apple.drawApple();
             }
@@ -29,7 +67,9 @@ class Game {
 
     endGame() {
         clearInterval(this.gameInterval);
+        this.score = 0;
+
+        // const restartGameButton = document.querySelector('#restart-game');
+        // restartGameButton.style.display = 'inline';       
     }
 }
-
-new Game();
